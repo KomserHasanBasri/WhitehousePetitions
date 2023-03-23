7215ef8ae8a7dccf2a6b1bfc79ca893d3a422d47
BasriKomser <101523343+BasriKomser@users.noreply.github.com>
2023-03-23 15:19:59 +0300

    Hello Whitehouse

---
+++ WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Hasan Basri Komser on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
