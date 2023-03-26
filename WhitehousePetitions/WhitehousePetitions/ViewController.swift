26546e2f1c9abee464c411e2d3fc0856bc08c486
BasriKomser <101523343+BasriKomser@users.noreply.github.com>
2023-03-27 01:10:33 +0300

    filter search

--- WhitehousePetitions/WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -9,12 +9,11 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petititons = [Petition]()
-
+    var filteredPetitions = [Petition]()
+    var urlString = String()
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredit))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(findPetition))
-        let urlString: String
+        createNavItems()
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
@@ -29,41 +28,36 @@ class ViewController: UITableViewController {
         }
             showError()
     }
-    @objc func showCredit() {
-        var message: String!
-        if navigationController?.tabBarItem.tag == 0 {
-            message = "The data comes from the We The People API (recent) of the Whitehouse."
-        } else {
-            message = "The data comes from the We The People API (rated) of the Whitehouse."
-        }
-        let ac = UIAlertController(title: "Attention!", message: message , preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .cancel))
-        present(ac,animated: true)
+    
+    func createNavItems() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredit))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(findPetition))
     }
+
     @objc func findPetition(){
-        let alert = UIAlertController(title: "Find", message:  "Enter the petition you want to find", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Find", message:  "Enter the petition you want to find \n\n Do an empty search to revert to the old view", preferredStyle: .alert)
         alert.addTextField { (textField) in
             textField.text = ""
         }
-        alert.addAction(UIAlertAction(title: "Find", style: .default,handler: nil))
+        alert.addAction(UIAlertAction(title: "Find", style: .default,handler: { _ in
+                if let searchText = alert.textFields?[0].text, !searchText.isEmpty {
+                    let filteredPetitions = self.petititons.filter { $0.title.lowercased().contains(searchText.lowercased()) }
+                    self.filteredPetitions = filteredPetitions
+                    self.petititons = filteredPetitions
+                    self.tableView.reloadData()
+                } else {
+                    if let url = URL(string: self.urlString) {
+                        if let data = try? Data(contentsOf: url) {
+                            self.parse(json: data)
+                            return
+                        }
+                    }
+                }
+            
+        }))
         self.present(alert, animated: true)
     }
    
-    
-    func parse(json: Data) {
-        let decoder = JSONDecoder()
-        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            petititons = jsonPetitions.results
-            tableView.reloadData()
-        }
-    }
-    
-    func showError() {
-        let ac = UIAlertController(title: "Loadin Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac,animated: true)
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petititons.count
     }
@@ -80,5 +74,30 @@ class ViewController: UITableViewController {
         let vc = DetailViewController()
         vc.detailItem = petititons[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func showCredit() {
+        var message: String!
+        if navigationController?.tabBarItem.tag == 0 {
+            message = "The data comes from the We The People API (RECENT) of the Whitehouse."
+        } else {
+            message = "The data comes from the We The People API (RATED) of the Whitehouse."
+        }
+        let ac = UIAlertController(title: "Attention!", message: message , preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+        present(ac,animated: true)
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loadin Error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac,animated: true)
+    }
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petititons = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
 }
